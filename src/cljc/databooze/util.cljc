872aa17b164bd28272147e7564a6211949b524b9
Andy Lu <luandy64@gmail.com>
2020-07-04 11:25:57 -0500

    Update /src

--- src/cljc/databooze/util.cljc
+++
@@ -1,6 +0,0 @@
-(ns databooze.util)
-
-(defn foo-cljc [x]
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
--- src/databooze/core.cljs
+++ src/databooze/core.cljs
@@ -1,11 +1,7 @@
-(ns databooze.core
+(ns ^:figwheel-hooks databooze.core
   (:require
-   [reagent.core :as reagent :refer [atom]]
-   [reagent.dom :as rdom]
-   [reagent.session :as session]
-   [reitit.frontend :as reitit]
-   [clerk.core :as clerk]
-   [accountant.core :as accountant]))
+   [goog.dom :as gdom]
+   [reagent.core :as reagent :refer [atom]]))
 
 ;; -------------------------
 ;; State
@@ -21,32 +17,6 @@
                          {:id 3
                           :drink-name "mint julip"
                           :ingredients ["ice" "bourbon" "mint" "simple syrup"]}]))
-
-
-;; -------------------------
-;; Routes
-
-(def router
-  (reitit/router
-   [["/" :index]
-    ["/items"
-     ["" :items]
-     ["/:item-id" :item]]
-    ["/about" :about]]))
-
-(defn path-for [route & [params]]
-  (if params
-    (:path (reitit/match-by-name router route params))
-    (:path (reitit/match-by-name router route))))
-
-;; -------------------------
-;; Page components
-
-(defn get-list-items []
-  [:ul
-   [:li [:a {:href (path-for :items)} "Items of databooze"]]
-   [:li [:a {:href "/broken/link"} "Broken link"]]
-   [:li [:p "Here is the drink you can make"]]])
 
 (defn save-input []
   (swap! item-id inc)
@@ -133,91 +103,25 @@
          [:span {:style {:margin-right 20}} (title-case (:drink-name drink))]
          [:span {:style {:margin-right 20}} "Missing: " (:missing drink)]]))]])
 
-(defn home-page []
-  (fn []
-    (let [
-          ]
-      [:span.main
-       [:h1 "Welcome to DataBooze"]
-       (get-user-input user-inputs current-input)
-       (things-i-have user-inputs)
-       (can-make)
-       (almost-make database user-inputs)])))
+(defn get-app-element []
+  (gdom/getElement "app"))
 
+(defn main-app []
+  [:span.main
+   [:h1 "Welcome to DataBooze"]
+   (get-user-input user-inputs current-input)
+   (things-i-have user-inputs)
+   (can-make)
+   (almost-make database user-inputs)])
 
+(defn mount [el]
+  (reagent.dom/render [main-app] el))
 
-(defn items-page []
-  (fn []
-    [:span.main
-     [:h1 "The items of databooze"]
-     [:ul (map (fn [item-id]
-                 [:li {:name (str "item-" item-id) :key (str "item-" item-id)}
-                  [:a {:href (path-for :item {:item-id item-id})} "Item: " item-id]])
-               (range 1 60))]]))
+(defn mount-app-element []
+  (when-let [el (get-app-element)]
+    (mount el)))
 
+(mount-app-element)
 
-(defn item-page []
-  (fn []
-    (let [routing-data (session/get :route)
-          item (get-in routing-data [:route-params :item-id])]
-      [:span.main
-       [:h1 (str "Item " item " of databooze")]
-       [:p [:a {:href (path-for :items)} "Back to the list of items"]]])))
-
-
-(defn about-page []
-  (fn [] [:span.main
-          [:h1 "About databooze"]]))
-
-
-;; -------------------------
-;; Translate routes -> page components
-
-(defn page-for [route]
-  (case route
-    :index #'home-page
-    :about #'about-page
-    :items #'items-page
-    :item #'item-page))
-
-
-;; -------------------------
-;; Page mounting component
-
-(defn current-page []
-  (fn []
-    (let [page (:current-page (session/get :route))]
-      [:div
-       [:header
-        [:p [:a {:href (path-for :index)} "Home"] " | "
-         [:a {:href (path-for :about)} "About databooze"]]]
-       [page]
-       [:footer
-        [:p "databooze was created by "
-         [:a {:href "https://github.com/luandy64"}
-          "Andy Lu"]]]])))
-
-;; -------------------------
-;; Initialize app
-
-(defn mount-root []
-  (rdom/render [current-page] (.getElementById js/document "app")))
-
-(defn init! []
-  (clerk/initialize!)
-  (accountant/configure-navigation!
-   {:nav-handler
-    (fn [path]
-      (let [match (reitit/match-by-path router path)
-            current-page (:name (:data  match))
-            route-params (:path-params match)]
-        (reagent/after-render clerk/after-render!)
-        (session/put! :route {:current-page (page-for current-page)
-                              :route-params route-params})
-        (clerk/navigate-page! path)
-        ))
-    :path-exists?
-    (fn [path]
-      (boolean (reitit/match-by-path router path)))})
-  (accountant/dispatch-current!)
-  (mount-root))
+(defn ^:after-load on-reload []
+  (mount-app-element))
